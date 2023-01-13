c218dddf1e2c10d961ff69fdc9761ad4cea75131
snow-webApp-004 <119712353+seoljoo-kang@users.noreply.github.com>
2023-01-13 17:12:33 +0900

    23.01.13
    주석 추가

--- Assets/Scripts/student_yes.cs
+++ Assets/Scripts/student_yes.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class student_yes : MonoBehaviour
 {
-    [SerializeField]
-    private Animator animator;
+    [SerializeField] //유니티 에디터에서의 접근 허용
+    private Animator animator; //Animator 클래스 변수 선언
 
-    public void Student_yes()
+    public void Student_yes() 
     {
-        animator.SetTrigger("student_yes");
+        animator.SetTrigger("student_yes"); //animator의 student_yes 트리거 작동
     }
 }
+
--- Assets/Scripts/timeline_play.cs
+++ Assets/Scripts/timeline_play.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
-using UnityEngine.Playables;
+using UnityEngine.Playables; //PlayableDirector 클래스 활용을 위한 네임스페이스 선언
 public class timeline_play : MonoBehaviour
 {
-    public PlayableDirector playableDirector;
+    public PlayableDirector playableDirector; //PlayableDirector 클래스 변수 선언
  
     public void Play()
     {
-        playableDirector.Play ();
+        playableDirector.Play (); //playableDirector을 통한 타임라인 실행
+
+        //Button의 부모 클래스에 해당하는 Panel을 비활성화 하기위한 코드
         gameObject.transform.parent.gameObject.SetActive(false); 
     }
 }
